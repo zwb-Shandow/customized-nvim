2768adfd1758b97d9f9ffba9a60d2d9aed3d5b26
zhangwenbo <zhangwenbo@trunk.tech>
2022-05-01 18:30:37 +0800

    feat: add keybindings

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 -- 基础设置
 require("basic")
+
+-- 快捷键设置
+require("keybindings")
---
+++ lua/keybindings.lua
@@ -0,0 +1,57 @@
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+
+local map = vim.api.nvim_set_keymap
+-- 复用 opt 参数
+local opt = { noremap = true, silent = true }
+
+-- 分屏快捷键
+map("n", "s", "", opt)
+map("n", "sv", ":vsp<CR>", opt)
+map("n", "sh", ":sp<CR>", opt)
+-- 关闭当前
+map("n", "sc", "<C-w>c", opt)
+-- 关闭其他
+map("n", "so", "<C-w>o", opt)
+
+-- 窗口跳转
+map("n", "<C-h>", "<C-w>h", opt)
+map("n", "<C-j>", "<C-w>j", opt)
+map("n", "<C-k>", "<C-w>k", opt)
+map("n", "<C-l>", "<C-w>l", opt)
+
+-- 窗口比例控制
+map("n", "<C-Left>", ":vertical resize -2<CR>", opt)
+map("n", "<C-Right>", ":vertical resize +2<CR>", opt)
+map("n", "<C-Up>", ":resize -2<CR>", opt)
+map("n", "<C-Down>", ":resize +2<CR>", opt)
+-- 等比例
+map("n", "s=", "<C-w>=", opt)
+
+-- terminal 相关
+map("n", "<leader>t", ":sp | terminal<CR>", opt)
+map("n", "<leader>vt", ":vsp | terminal<CR>", opt)
+map("t", "<C-h>", [[ <C-\><C-N><C-w>h ]], opt)
+map("t", "<C-j>", [[ <C-\><C-N><C-w>j ]], opt)
+map("t", "<C-k>", [[ <C-\><C-N><C-w>k ]], opt)
+map("t", "<C-l>", [[ <C-\><C-N><C-w>l ]], opt)
+
+-- visual 模式下缩进代码
+map("v", "<", "<gv", opt)
+map("v", ">", ">gv", opt)
+-- 上下移动选中文本
+map("v", "J", ":move '>+1<CR>gv-gv", opt)
+map("v", "K", ":move '<-2<CR>gv-gv", opt)
+
+-- 上下滚动浏览
+-- cutl+u / curl+d 只移动9行，默认移动半屏
+map("n", "<C-u>", "9k", opt)
+map("n", "<C-d>", "9j", opt)
+
+-- 在visual模式里粘贴不要复制
+map("v", "p", '"_dP', opt)
+
+-- 退出
+map("n", "q", ':q<CR>', opt)
+map("n", "qq", ':q!<CR>', opt)
+map("n", "Q", ':qa!<CR>', opt)
