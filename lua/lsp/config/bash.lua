54eca08148290e965b6c68cc9c6d3a0298cab5d0
zhangwenbo <zhangwenbo@trunk.tech>
2022-05-09 13:15:22 +0800

    fix: 适配neovim v0.8.0 api

--- lua/lsp/config/bash.lua
+++ lua/lsp/config/bash.lua
@@ -1,6 +1,13 @@
 local opts = {
 	settings = nil,
-	on_attach = nil,
+	on_attach = function(client, bufnr)
+		-- 禁用格式化功能，交给专门插件处理
+		client.server_capabilities.document_formatting = false
+		client.server_capabilities.document_range_formatting = false
+
+		-- 保存时自动格式化
+		-- vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.formatting_sync()")
+	end,
 }
 
 return {
